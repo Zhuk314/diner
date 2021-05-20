18eb912b30ca8c3b563ab778bd661f50b6f7fb1f
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-05-20 12:15:49 -0400

    generate getters and setters

--- classes/order.php
+++ classes/order.php
@@ -14,4 +14,53 @@ class Order
         $this->_food = "";
     }
 
+    /**
+     * @return string
+     */
+    public function getFood(): string
+    {
+        return $this->_food;
+    }
+
+    /**
+     * @param string $food
+     */
+    public function setFood(string $food): void
+    {
+        $this->_food = $food;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getMeel()
+    {
+        return $this->_meel;
+    }
+
+    /**
+     * @param mixed $meel
+     */
+    public function setMeel($meel): void
+    {
+        $this->_meel = $meel;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCondiments()
+    {
+        return $this->_condiments;
+    }
+
+    /**
+     * @param mixed $condiments
+     */
+    public function setCondiments($condiments): void
+    {
+        $this->_condiments = $condiments;
+    }
+
+
 }
