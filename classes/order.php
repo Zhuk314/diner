016fdbc9715833d2d781f2e1281d893776e8d25d
Yurii Zhuk <iamyuriizhuk@gmail.com>
2021-05-20 12:13:59 -0400

    Create order object

---
+++ classes/order.php
@@ -0,0 +1,17 @@
+<?php
+
+class Order
+{
+    private $_food;
+    private $_meel;
+    private $_condiments;
+
+    /**
+     * Order constructor.
+     */
+    public function __construct()
+    {
+        $this->_food = "";
+    }
+
+}
